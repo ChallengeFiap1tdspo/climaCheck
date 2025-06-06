e06b19f26bffc8a44055030ee3bb4cf956992c97
Paulo-Estalise <phestalise@gmail.com>
2025-06-06 14:48:20 -0300

    adiciona estrutura inicial e captura de elementos do formulário de cadastro

---
+++ js/cadastro.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("registerForm");
+  const nameInput = document.getElementById("registerName");
+  const emailInput = document.getElementById("registerEmail");
+  const passwordInput = document.getElementById("registerPassword");
+  const confirmPasswordInput = document.getElementById("registerConfirmPassword");
+
+  const nameError = document.getElementById("registerNameError");
+  const emailError = document.getElementById("registerEmailError");
+  const passwordError = document.getElementById("registerPasswordError");
+  const confirmPasswordError = document.getElementById("registerConfirmPasswordError");
+});
