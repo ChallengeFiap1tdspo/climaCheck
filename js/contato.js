2fb2a2432f1b80932e6ea7baa1dadc64c0c6cf8c
Paulo-Estalise <phestalise@gmail.com>
2025-06-06 14:55:12 -0300

    adiciona estrutura inicial e captura de campos do formulário de contato e cria o arquivo contato-js

---
+++ js/contato.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const contatoForm = document.getElementById("contatoForm");
+
+  const nameInput = document.getElementById("name");
+  const emailInput = document.getElementById("email");
+  const cepInput = document.getElementById("cep");
+  const subjectInput = document.getElementById("subject");
+  const messageInput = document.getElementById("message");
+
+  contatoForm.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    
+  });
+});
