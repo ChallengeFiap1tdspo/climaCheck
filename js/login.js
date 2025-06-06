d439d7fb9554d82785a880b7baf80dfc5d11d65c
Paulo-Estalise <phestalise@gmail.com>
2025-06-06 14:39:12 -0300

    adiciona estrutura inicial para capturar elementos do formulário

---
+++ js/login.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("loginForm");
+  const emailInput = document.getElementById("loginEmail");
+  const passwordInput = document.getElementById("loginPassword");
+  const emailError = document.getElementById("loginEmailError");
+  const passwordError = document.getElementById("loginPasswordError");
+});
