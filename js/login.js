8f2817156f344b7f44ec8082f28b847f6f5c354c
Paulo-Estalise <phestalise@gmail.com>
2025-06-06 14:43:15 -0300

    adiciona alerta de sucesso e envio do formulário quando válido

--- js/login.js
+++ js/login.js
@@ -4,12 +4,38 @@ document.addEventListener("DOMContentLoaded", function () {
   const passwordInput = document.getElementById("loginPassword");
   const emailError = document.getElementById("loginEmailError");
   const passwordError = document.getElementById("loginPasswordError");
-  function validateEmail(email) {
-  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  return regex.test(email);
-}
-form.addEventListener("submit", function (e) {
-  e.preventDefault(); 
-});
 
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    let isValid = true;
+    emailError.textContent = "";
+    passwordError.textContent = "";
+
+    const emailValue = emailInput.value.trim();
+    const passwordValue = passwordInput.value.trim();
+
+
+    if (!validateEmail(emailValue)) {
+      emailError.textContent = "Digite um e-mail válido.";
+      isValid = false;
+    }
+
+
+    if (passwordValue.length < 6) {
+      passwordError.textContent = "A senha deve ter pelo menos 6 caracteres.";
+      isValid = false;
+    }
+
+    if (isValid) {
+      alert("Login realizado com sucesso!");
+      form.submit(); 
+    }
+  });
+
+  function validateEmail(email) {
+ 
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+  }
 });
