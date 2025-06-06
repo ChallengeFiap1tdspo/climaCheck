8e7e7e77efcf5e9dab641d199d529a5d2a7359d9
Paulo-Estalise <phestalise@gmail.com>
2025-06-06 14:08:30 -0300

    estrutura inicial do script e seleção dos elementos DOM

---
+++ js/consultar.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const form = document.getElementById("consultaForm");
+  const cepInput = document.getElementById("cep");
+  const regiaoSelect = document.getElementById("regiao");
+  const climaSelect = document.getElementById("clima");
+  const weatherAlert = document.getElementById("weatherAlert");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
