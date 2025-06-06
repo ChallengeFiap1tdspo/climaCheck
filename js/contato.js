0a1e6c5de86618bdaf5b94a79661da3ea79dc832
Paulo-Estalise <phestalise@gmail.com>
2025-06-06 14:57:33 -0300

    adiciona função utilitária para validar e-mail

--- js/contato.js
+++ js/contato.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
   contatoForm.addEventListener("submit", function (e) {
     e.preventDefault();
 
-        let isValid = true;
+    let isValid = true;
     let errorMessage = "";
 
     const nameValue = nameInput.value.trim();
@@ -19,31 +19,26 @@ document.addEventListener("DOMContentLoaded", function () {
     const subjectValue = subjectInput.value;
     const messageValue = messageInput.value.trim();
 
- 
     if (nameValue.length < 3) {
       errorMessage += "- O nome deve ter pelo menos 3 caracteres.\n";
       isValid = false;
     }
-
 
     if (!validateEmail(emailValue)) {
       errorMessage += "- Digite um e-mail válido.\n";
       isValid = false;
     }
 
-
     if (cepValue && cepValue.length !== 8) {
       errorMessage += "- O CEP deve conter exatamente 8 números.\n";
       isValid = false;
     }
-
 
     if (!subjectValue) {
       errorMessage += "- Selecione um assunto.\n";
       isValid = false;
     }
 
-   
     if (messageValue.length < 10) {
       errorMessage += "- A mensagem deve conter pelo menos 10 caracteres.\n";
       isValid = false;
@@ -55,6 +50,10 @@ document.addEventListener("DOMContentLoaded", function () {
       alert("Mensagem enviada com sucesso! Obrigado pelo contato.");
       contatoForm.reset();
     }
-
   });
 });
+
+function validateEmail(email) {
+  const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return regex.test(email);
+}
