82324c9fbf3e4217fabbf44c94cbe90f06d16a8a
Paulo-Estalise <phestalise@gmail.com>
2025-06-06 14:49:20 -0300

    adiciona validação de campos e tratamento de erros

--- js/cadastro.js
+++ js/cadastro.js
@@ -9,4 +9,50 @@ document.addEventListener("DOMContentLoaded", function () {
   const emailError = document.getElementById("registerEmailError");
   const passwordError = document.getElementById("registerPasswordError");
   const confirmPasswordError = document.getElementById("registerConfirmPasswordError");
+  form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let isValid = true;
+
+  
+  nameError.textContent = "";
+  emailError.textContent = "";
+  passwordError.textContent = "";
+  confirmPasswordError.textContent = "";
+
+  const nameValue = nameInput.value.trim();
+  const emailValue = emailInput.value.trim();
+  const passwordValue = passwordInput.value.trim();
+  const confirmPasswordValue = confirmPasswordInput.value.trim();
+
+
+  if (nameValue.length < 3) {
+    nameError.textContent = "O nome deve ter pelo menos 3 caracteres.";
+    isValid = false;
+  }
+
+
+  if (!validateEmail(emailValue)) {
+    emailError.textContent = "Digite um e-mail válido.";
+    isValid = false;
+  }
+
+  
+  if (passwordValue.length < 6) {
+    passwordError.textContent = "A senha deve ter pelo menos 6 caracteres.";
+    isValid = false;
+  }
+
+
+  if (passwordValue !== confirmPasswordValue) {
+    confirmPasswordError.textContent = "As senhas não coincidem.";
+    isValid = false;
+  }
+
+  if (isValid) {
+    alert("Cadastro realizado com sucesso!");
+    form.submit(); 
+  }
+});
+
 });
